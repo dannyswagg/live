190e67b8ddd63235f0d4b80a0c69f095472e0963
dannyswagg <ugiomohd@yahoo.com>
2024-11-02 12:46:17 +0100

    bck

--- server/index.js
+++ server/index.js
@@ -3,25 +3,27 @@ const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-app.use(cors());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173", // Use CLIENT_URL for production, fallback for local
+    methods: ["GET", "POST"],
+  })
+);
 
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
-    method: ["POST", "GET"],
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    methods: ["POST", "GET"],
   },
 });
 
 io.on("connection", (socket) => {
-  console.log(`User Connected ${socket.id}`);
+  console.log(`User Connected: ${socket.id}`);
   socket.on("send_message", (data) => {
     socket.broadcast.emit("receive_message", data);
   });
 });
 
-server.listen(5174, () => {
-  console.log("Server is Running");
-});
-
-module.exports = server; // Export the server for Vercel
+module.exports = server; // Export server for Vercel
